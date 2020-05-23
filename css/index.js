3ff70936876bed3f06a40c86c909df1d05e668f0
Knakna <kna@tut.by>
2020-05-23 17:36:27 +0300

    openedu courses

---
+++ css/index.js
@@ -0,0 +1,157 @@
+.contacts - form {
+    float: left;
+    width: 670 px;
+    /* margin-bottom: 0px;
+    padding-bottom: 15px; */
+    /* margin-right: 50px; */
+}
+
+
+/* .contacts-form table {
+  border: none;
+  box-sizing: border-box;
+  border-collapse: collapse; */
+
+.contacts - form td {
+    padding - bottom: 25 px;
+}
+
+.contacts - form.title {
+        width: 94 px;
+        padding - right: 11 px;
+        padding - left: 0 px;
+        vertical - align: top;
+        text - align: right;
+        /* font-size: 18px; */
+        /* line-height: 24px; */
+        font - weight: bold;
+        /* font-family: 'Trebuchet MS', sans-serif; */
+        color: #000;
+
+  /* vertical-align: middle; */
+
+}
+
+
+
+.contacts-form .field {
+
+  width: 510px;
+
+}
+
+
+
+.contacts-form input[type= "text"],
+    .contacts - form textarea {
+        -webkit - box - sizing: border - box; -
+        moz - box - sizing: border - box;
+        box - sizing: border - box;
+        width: 100 % ;
+        padding: 16 px 30 px;
+        /* font-weight: 400;
+        font-size: 18px; */
+        font - family: Tahoma, sans - serif;
+        /* line-height: 24px; */
+        outline: none;
+        border: 2 px solid# a7a7a7;
+        /* border-radius: 2px; */
+        background - color: #fff;
+        /* margin-bottom: 15px; */
+        /* padding-bottom: 17px; */
+    }
+
+.contacts - form textarea {
+    min - height: 105 px;
+    resize: none;
+}
+
+.contacts - form.btn - contacts {
+    width: 100 % ;
+    /* margin-bottom: 30px; */
+}
+
+.contacts - instruction {
+    float: right;
+    width: 320 px;
+    margin - bottom: 17 px;
+}
+
+.contacts - instruction h3 {
+    margin: 0;
+    margin - bottom: 25 px;
+    font - size: 22 px;
+    line - height: 30 px;
+}
+
+.contacts - instruction p {
+    margin: 0;
+    margin - bottom: 25 px;
+    color: #555555;
+
+  font-size: 18px;
+
+  line-height: 24px;
+
+  font-family: Tahoma, sans-serif;
+
+}
+
+
+
+.contacts-instruction .contacts-alert {
+
+  margin-top: 40px;
+
+  margin-bottom: 0;
+
+}
+
+
+
+.contacts-alert strong {
+
+  color: # 000000;
+    font - weight: bold;
+}
+
+
+
+.contacts-form input[type="radio"] {
+  padding-left: 55px;
+
+}
+.contacts-form input[id="man"] {
+  padding-left: 25px;
+
+}
+.contacts-form label input[id="woman"] {
+  padding-left: 25px;
+
+}
+
+
+.features-icon {
+  position: absolute;
+  top: -5px;
+  left: 125px;
+  background-image: url(../img/sprite.png);
+}
+
+.features-icon-schedule {
+  width: 65px;
+  height: 65px;
+  background-position: -6px 3px;
+}
+
+.features-icon-practice {
+  width: 80px;
+  height: 77px;
+  background-position: -354px 3px;
+}
+
+.features-icon-work {
+  width: 75px;
+  height: 70px;
+  background-position: 78px 0px;
+}
